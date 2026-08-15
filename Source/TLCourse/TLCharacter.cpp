db448f7cac39df6ce207e58f12ba10f67de61167
fdezbarroso <f.fdezbarroso@gmail.com>
2026-08-15 19:51:45 +0200

    L2.2 Add camera and springarm to character, change some settings

--- Source/TLCourse/TLCharacter.cpp
+++ Source/TLCourse/TLCharacter.cpp
@@ -1,14 +1,19 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "TLCharacter.h"
+
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
 
 // Sets default values
 ATLCharacter::ATLCharacter()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
-
+	
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
 }
 
 // Called when the game starts or when spawned
@@ -31,4 +36,3 @@ void ATLCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputCompone
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
 }
-
--- Source/TLCourse/TLCharacter.h
+++ Source/TLCourse/TLCharacter.h
@@ -1,10 +1,11 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "TLCharacter.generated.h"
+
+class USpringArmComponent;
+class UCameraComponent;
 
 UCLASS()
 class TLCOURSE_API ATLCharacter : public ACharacter
@@ -16,6 +17,13 @@ public:
 	ATLCharacter();
 
 protected:
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<UCameraComponent> CameraComponent;
+	
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	TObjectPtr<USpringArmComponent> SpringArmComponent;
+	
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
